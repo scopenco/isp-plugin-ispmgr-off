4f5b2276c6087dc38a00331b4820f986e2aee2a1
Andrey Scopenco <andrey@scopenco.net>
2014-01-31 11:53:02 +0400

    pep8 style

--- addon/ispmgr_off.py
+++ addon/ispmgr_off.py
@@ -10,39 +10,46 @@ PLUGIN_NAME = 'ispmgr_off'
 LOG_FILE = '/usr/local/ispmgr/var/ispmgr.log'
 
 from xml.dom import minidom
-from os import chdir,getpid,environ,access,R_OK,chown,listdir
-from sys import exit,stderr
+from os import chdir, getpid, environ, access, R_OK, chown, listdir
+from sys import exit, stderr
 from cgi import FieldStorage
 from traceback import format_exc
 
+
 class ExitOk(Exception):
     pass
-       
+
+
 class Log(object):
     '''Class used for add debug to ispmgr.log'''
     def __init__(self, plugin=None, output=LOG_FILE):
         import time
         timenow = time.localtime(time.time())
         self.timef = time.strftime("%b %d %H:%M:%S", timenow)
-        self.log = output;
-        self.plugin_name = plugin;
+        self.log = output
+        self.plugin_name = plugin
         self.fsock = open(self.log, 'a+')
         self.pid = getpid()
         self.script_name = __file__
 
-    def write(self,desc):
+    def write(self, desc):
         if not (desc == "\n"):
             if (desc[-1:] == "\n"):
-                self.fsock.write('%s [%s] ./%s \033[36;40mPLUGIN %s :: %s\033[0m' % \
-                    (self.timef, self.pid, self.script_name, self.plugin_name, desc))
+                self.fsock.write(
+                    '%s [%s] ./%s \033[36;40mPLUGIN %s :: %s\033[0m' % (
+                        self.timef, self.pid, self.script_name,
+                        self.plugin_name, desc))
             else:
-                self.fsock.write('%s [%s] ./%s \033[36;40mPLUGIN %s :: %s\033[0m\n' % \
-                    (self.timef, self.pid, self.script_name, self.plugin_name, desc))
+                self.fsock.write(
+                    '%s [%s] ./%s \033[36;40mPLUGIN %s :: %s\033[0m\n' % (
+                        self.timef, self.pid, self.script_name,
+                        self.plugin_name, desc))
 
     def close(self):
         self.fsock.close()
 
-def xml_doc(elem = None, text = None):
+
+def xml_doc(elem=None, text=None):
     xmldoc = minidom.Document()
     doc = xmldoc.createElement('doc')
     xmldoc.appendChild(doc)
@@ -54,7 +61,8 @@ def xml_doc(elem = None, text = None):
             emp.appendChild(msg_text)
     return xmldoc.toxml('UTF-8')
 
-def xml_error(text,code_num=None):
+
+def xml_error(text, code_num=None):
     xmldoc = minidom.Document()
     doc = xmldoc.createElement('doc')
     xmldoc.appendChild(doc)
@@ -63,16 +71,16 @@ def xml_error(text,code_num=None):
     if code_num:
         code = xmldoc.createAttribute('code')
         error.setAttributeNode(code)
-        error.setAttribute('code',str(code_num))
-        if code_num in [2,3,6]:
+        error.setAttribute('code', str(code_num))
+        if code_num in [2, 3, 6]:
             obj = xmldoc.createAttribute('obj')
             error.setAttributeNode(obj)
-            error.setAttribute('obj',str(text))
+            error.setAttribute('obj', str(text))
             return xmldoc.toxml('UTF-8')
-        elif code_num in [4,5]:                                                                                                                                                                
+        elif code_num in [4, 5]:
             val = xmldoc.createAttribute('val')
             error.setAttributeNode(val)
-            error.setAttribute('val',str(text))
+            error.setAttribute('val', str(text))
             return xmldoc.toxml('UTF-8')
     error_text = xmldoc.createTextNode(text.decode('utf-8'))
     error.appendChild(error_text)
